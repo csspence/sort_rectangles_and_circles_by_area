333459fcea3e30c8997e4f1892b9ab300b72ca3e
Crew Spence <csspence123@gmail.com>
2020-04-18 05:33:46 -0500

    Wrote working solution.

--- sort_rectangles_and_circles_by_area.js
+++ sort_rectangles_and_circles_by_area.js
@@ -8,6 +8,13 @@ const array = [ [4.23, 6.43], 1.23, 3.444, [1.342, 3.212] ]; // [ rectangle, cir
 sortByArea(array) => [ [ 1.342, 3.212 ], 1.23, [ 4.23, 6.43 ], 3.444 ]
 */
 
-function sortByArea(array) {
-  return array;
+const area = (shape) => {
+  if (typeof shape === 'object')
+    return shape[0] * shape[1];
+  else
+    return Math.PI * Math.pow(shape, 2);
+}
+
+const sortByArea = (array) => {
+  return array.slice(0).sort((a, b) => area(a) - area(b));
 }
